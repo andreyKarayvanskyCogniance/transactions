8d6dc838349b78c55f0dc48f61a2d1da35f42d15
Andrey Karayvansky <akarayvansky@cogniance.com>
2014-10-01 10:41:16 +0300

    Generating random values for test string data.

--- transactions-web/src/main/java/demo/Application.java
+++ transactions-web/src/main/java/demo/Application.java
@@ -3,6 +3,8 @@ package demo;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -24,35 +26,39 @@ public class Application {
 
         // TODO remove test code
         final AccountRepository accountRepository = context.getBean(AccountRepository.class);
-        
-        
+
         for (int i = 0; i < 100; i++) {
             final Account demoAccount = new Account();
-            demoAccount.setCode("demo" + i);
-            demoAccount.setName("Demo Account " + i);
-            
+            demoAccount.setCode(randomStr("C ", 20));
+            demoAccount.setName(randomStr("N ", 50));
+
             accountRepository.save(demoAccount);
-            
+
             final ThreadLocalRandom random = ThreadLocalRandom.current();
-            
+
             if (random.nextBoolean()) {
                 for (int j = 0; j < random.nextInt(0, 100); j++) {
 
                     final TransactionRepository transactionRepository = context.getBean(TransactionRepository.class);
-                    
+
                     final Transaction transaction = new Transaction();
                     transaction.setAmount(new BigDecimal(random.nextDouble(-100.0d, 100.0d)));
                     transaction.setAccount(demoAccount);
                     transaction.setDate(new Date());
-                    transaction.setDescription("A Demo Transaction " + i + " for account '" + demoAccount.getCode()
-                            + "'");
+                    transaction.setDescription(randomStr("T ", 100));
                     transaction.setType(random.nextBoolean() ? TransactionType.CREDIT : TransactionType.DEBIT);
-                    
+
                     transactionRepository.save(transaction);
-                    
+
                 }
             }
         }
 
+    }
+
+    private static String randomStr(String token, int bound) {
+        int len = ThreadLocalRandom.current().nextInt(1, bound / 2);
+
+        return Stream.generate(() -> token).limit(len).collect(Collectors.joining());
     }
 }
