1769af19946bf0c8b32f6769615c7a460faed22e
Andrey Karayvansky <akarayvansky@cogniance.com>
2014-10-01 09:59:00 +0300

    Added more test data.

--- transactions-web/src/main/java/demo/Application.java
+++ transactions-web/src/main/java/demo/Application.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -24,21 +25,34 @@ public class Application {
         // TODO remove test code
         final AccountRepository accountRepository = context.getBean(AccountRepository.class);
         
-        final Account demoAccount = new Account();
-        demoAccount.setCode("demo1");
-        demoAccount.setName("The Very First Demo Account");
         
-        accountRepository.save(demoAccount);
+        for (int i = 0; i < 100; i++) {
+            final Account demoAccount = new Account();
+            demoAccount.setCode("demo" + i);
+            demoAccount.setName("Demo Account " + i);
+            
+            accountRepository.save(demoAccount);
+            
+            final ThreadLocalRandom random = ThreadLocalRandom.current();
+            
+            if (random.nextBoolean()) {
+                for (int j = 0; j < random.nextInt(0, 100); j++) {
 
-        final TransactionRepository transactionRepository = context.getBean(TransactionRepository.class);
-        
-        final Transaction transaction = new Transaction();
-        transaction.setAmount(new BigDecimal(10.0d));
-        transaction.setAccount(demoAccount);
-        transaction.setDate(new Date());
-        transaction.setDescription("A Demo Transaction");
-        transaction.setType(TransactionType.DEBIT);
-        
-        transactionRepository.save(transaction);
+                    final TransactionRepository transactionRepository = context.getBean(TransactionRepository.class);
+                    
+                    final Transaction transaction = new Transaction();
+                    transaction.setAmount(new BigDecimal(random.nextDouble(-100.0d, 100.0d)));
+                    transaction.setAccount(demoAccount);
+                    transaction.setDate(new Date());
+                    transaction.setDescription("A Demo Transaction " + i + " for account '" + demoAccount.getCode()
+                            + "'");
+                    transaction.setType(random.nextBoolean() ? TransactionType.CREDIT : TransactionType.DEBIT);
+                    
+                    transactionRepository.save(transaction);
+                    
+                }
+            }
+        }
+
     }
 }
